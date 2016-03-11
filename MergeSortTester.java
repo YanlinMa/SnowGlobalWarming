b2839f62decf3ff49b722fc92688476e398091f9
Grace Stempel <gracestempel@Katara.home>
2016-03-10 23:37:18 -0500

    Tester functions complete

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -28,14 +28,51 @@ public class MergeSortTester
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
+
+    public static long timer(int[] a) {
+	long start = System.nanoTime();
+	MergeSort.sort(a);
+	long end = System.nanoTime();
+	return end - start;
+    }
+
     public static void main( String[] args ) 
     {
-	long startTime = System.currentTimeMillis();
-	//Insert MergeSort fxn with BLANK elements
-	long endTime = System.currentTimeMillis();
 
-	long elapsedTime = endTime - startTime;
-	System.out.print("Time for BLANK elements: " + elapsedTime);
+	int[] ten = new int[10];
+	int[] hun = new int[100];
+	int[] thou = new int[1000];
+	int[] tenThou = new int[10000];
+	int[] hunThou = new int[100000];
+	int[] milli = new int[1000000];
+
+	long sum1 = 0;
+	long sum2 = 0;
+	long sum3 = 0;
+	long sum4 = 0;
+	long sum5 = 0;
+	long sum6 = 0;
+
+	int counter = 100; //number of repeated executions
+	while (counter > 0) {
+
+	    sum1 += timer(ten);
+	    sum2 += timer(hun);
+	    sum3 += timer(thou);
+	    sum4 += timer(tenThou);
+	    sum5 += timer(hunThou);
+	    sum6 += timer(milli);
+
+	    counter --;
+	}
+
+	System.out.println("Average time for n=10: " + sum1/10.0);
+	System.out.println("Average time for n=100: " + sum2/10.0);
+	System.out.println("Average time for n=1000: " + sum3/10.0);
+	System.out.println("Average time for n=10000: " + sum4/10.0);
+	System.out.println("Average time for n=100000 " + sum5/10.0);
+	System.out.println("Average time for n=1000000 " + sum6/10.0);
+
     }//end main
 
 }//end class
